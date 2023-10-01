b79530dd6293ce412092ed671fe5b6ce533b8e49
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-10-01 13:57:04 -0600

    first commit

---
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -0,0 +1,53 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.direccion.Direccion;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String documento;
+    @Embedded
+    private Direccion direccion;
+    private Boolean activo;
+
+    public Paciente(DatosRegistroPaciente datosRegistroPaciente){
+        this.activo = true;
+        this.nombre = datosRegistroPaciente.nombre();
+        this.email = datosRegistroPaciente.email();
+        this.telefono = datosRegistroPaciente.telefono();
+        this.documento = datosRegistroPaciente.documento();
+        this.direccion = new Direccion(datosRegistroPaciente.datosDireccion());
+    }
+
+    public void actualizarInformacion(DatosActualizacionPaciente datos){
+        if(datos.nombre() != null){
+            this.nombre = datos.nombre();
+        }
+        if (datos.telefono() != null){
+            this.telefono = datos.telefono();
+        }
+        if (datos.direccion() != null){
+            this.direccion.actualizarDatos(datos.direccion());
+        }
+    }
+
+    public void eliminar(){
+        this.activo = false;
+    }
+
+}
